618881e404954068f6b8a21c4ba55902dbe2dae5
sofich97 <sofich416@gmail.com>
2022-09-14 23:08:07 +0300

    create function handleDelete create function renderPhrase

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -6,75 +6,75 @@ const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll());
 
     const handleDelete = (userId) => {
-
+        setUsers((prevState) => prevState.filter((user) => user !== userId));
     };
-    console.log(users[0].profession.name)
-    console.log(users[0].qualities.tedious)
 
-    const renderPhrase = () => {
-        return (
-            users.length !== 0 &&
-            users.map((user, index) => (
-                <>
-                    <tr>
-                        {/*<th key={user[index]['profession']['name']}>{user[index]['profession']['name']}</th>*/}
-                        <th key={user._id}>{user.name}</th>
-                    </tr>
-                    {/*<tr>*/}
-                    {/*    <th key={user[index].qualities}>{user.qualities.name}</th>*/}
-                    {/*</tr>*/}
-                    {/*<tr>*/}
-                    {/*    <th key={user[index].profession}>{user.profession.name}</th>*/}
-                    {/*</tr>*/}
-                    {/*<tr>*/}
-                    {/*    <th key={user[index].completedMeetings}>{user.completedMeetings}</th>*/}
-                    {/*</tr>*/}
-                    {/*<tr>*/}
-                    {/*    <th key={user[index].rate}>{user.rate}</th>*/}
-                    {/*</tr>*/}
 
-                    {/*<th key={[user]['qualities']}>{[user]['qualities']}</th>*/}
-                    {/*<th key={user.profession}>{user.profession}</th>*/}
-                    {/*<th key={user.completedMeetings}>{user.completedMeetings}</th>*/}
-                    {/*<th key={user.rate}>{user.rate}</th>*/}
-                </>
-
-            ))
-        );
-    };
-    // if (users.length !== 0) {
-    //     return <th>{renderPhrase()}</th>
-    // }
+    const renderPhrase = (phrase) => {
+        //let phrase;
+        if((users.length >= 2 && users.length <= 4) ) {
+            phrase = `${users.length} человека тусанут с тобой сегодня`
+        } else if((users.length === 1) || (users.length > 4)) {
+            phrase = `${users.length} человек тусанет с тобой сегодня`;
+        } else if(users.length === 0){
+            phrase = `Никто с тобой не тусанет`
+        }
+        return phrase;
+    }
+    const getBadgeClasses = () => {
+        let classes = 'badge m-3 ';
+        classes += users.length === 0 ? 'bg-danger' : 'bg-primary';
+        return classes;
+    }
 
 
 
     return (
         <>
-            <h1 className='badge bg-primary '>12 человек тусанет с тобой сегодня</h1>
-            <table className='table'>
-                <thead>
-                <tr>
-                    <th scope='col'>Имя</th>
-                    <th scope='col'>Качества</th>
-                    <th scope='col'>Профессия</th>
-                    <th scope='col'>Встретиться раз</th>
-                    <th scope='col'>Оценка</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    {/*{renderPhrase()}*/}
-                    {/*<td key={users}>{setUsers}</td>*/}
-                    {/*<td></td>*/}
-                    {/*<td></td>*/}
-                    {/*<td></td>*/}
-                    <td><button className='btn btn-danger'>delete</button></td>
-                </tr>
-                </tbody>
-            </table>
+            <h1 className={getBadgeClasses()}>{renderPhrase()}</h1>
+            {users.length !== 0 &&  (
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th scope='col'>Имя</th>
+                            <th scope='col'>Качества</th>
+                            <th scope='col'>Профессия</th>
+                            <th scope='col'>Встретиться раз</th>
+                            <th scope='col'>Оценка</th>
+                            <th scope='col'></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <tr key={user._id}>
+                                <td>{user.name}</td>
+                                <td>
+                                    {user.qualities.map((quality) => (
+                                        <span
+                                            key={quality._id}
+                                            className={`badge bg-${quality.color} m-1`}
+                                        >
+                                            {quality.name}
+                                        </span>
+                                    ))}
+                                </td>
+                                <td>{user.profession.name}</td>
+                                <td>{user.completedMeetings}</td>
+                                <td>{`${user.rate}/5`}</td>
+                                <td>
+                                    <button className='btn btn-danger'
+                                            onClick={() => handleDelete(user)}
+                                    >
+                                        delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </>
-    )
+    );
 };
 
 export default Users;
